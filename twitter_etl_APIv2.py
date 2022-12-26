c1a966fd5aa3cd4db7104f105bec13dbf9e7d3f5
Mies Dela Cruz <mieseliel.delacruz@gmail.com>
2022-12-26 21:58:04 +0800

    #3 - Finish APIv2 ETL script

--- twitter_etl_APIv2.py
+++ twitter_etl_APIv2.py
@@ -16,20 +16,27 @@ consumer_secret = lines[3].rstrip()
 #Twitter Authentication
 api = tweepy.Client(consumer_key=consumer_key, consumer_secret=consumer_secret, access_token=access_key, access_token_secret=access_secret)
 
-#API object
-
+#user id lookup
 user_id_lookup = api.get_user(username = "BillGates", user_auth = True)
 user_id = user_id_lookup[0]['id']
 
+#API object
 tweets = api.get_users_tweets(id = user_id, max_results = 100, user_auth = True)
 
+
 tweet_list = []
-# for tweet in tweets:
-#   text = tweet._json["full_text"]
+
+#Iterating over the API response
+for tweet in range(len(tweets[0])):
   
-#   refined_tweet = {
-#     "user"
-#   }
-  
-  
-print(tweets)
+  refined_tweet = {
+    "user": user_id,
+    "tweet_id": tweets[0][tweet]['id'],
+    "text": tweets[0][tweet]['text'],
+  }
+  tweet_list.append(refined_tweet)
+
+
+#Creating the CSV file    
+df = pd.DataFrame(tweet_list)
+df.to_csv("billgates_tweeter_data.csv")
